07aa6169ed3656bd6f43f915461dcc664b718031
NoranBESTEAM <nmohammed@be-steam.com>
2024-03-05 10:46:01 +0200

    Valuable Component

--- src/components/about/Valuable.vue
+++ src/components/about/Valuable.vue
@@ -10,10 +10,10 @@
 
             <!-- In Large Screen -->
 
-            <div class="row g-3 d-none d-sm-flex">
+            <div class="row g-3 d-none d-md-flex">
 
-                <div class="col-sm-6 col-xl-3 wow animate__animated animate__slideInUp animate__slower" data-wow-offset="50"
-                    data-wow-delay="0.1s">
+                <div class="col-md-6 col-xl-3 wow animate__animated animate__slideInUp animate__slower"
+                    data-wow-offset="50" data-wow-delay="0.1s">
 
                     <div class="box overflow-hidden" @mouseover="firstImg = false; firstTitle = false;"
                         @mouseleave="firstImg = true; firstTitle = true;">
@@ -34,8 +34,8 @@
 
                 </div>
 
-                <div class="col-sm-6 col-xl-3 wow animate__animated animate__slideInUp animate__slower" data-wow-offset="50"
-                    data-wow-delay="0.3s">
+                <div class="col-md-6 col-xl-3 wow animate__animated animate__slideInUp animate__slower"
+                    data-wow-offset="50" data-wow-delay="0.3s">
 
                     <div class="box overflow-hidden" @mouseover="secondImg = false; secondTitle = false;"
                         @mouseleave="secondImg = true; secondTitle = true;">
@@ -56,8 +56,8 @@
 
                 </div>
 
-                <div class="col-sm-6 col-xl-3 wow animate__animated animate__slideInUp animate__slower" data-wow-offset="50"
-                    data-wow-delay="0.5s">
+                <div class="col-md-6 col-xl-3 wow animate__animated animate__slideInUp animate__slower"
+                    data-wow-offset="50" data-wow-delay="0.5s">
 
                     <div class="box overflow-hidden" @mouseover="thirdImg = false; thirdTitle = false;"
                         @mouseleave="thirdImg = true; thirdTitle = true;">
@@ -78,8 +78,8 @@
 
                 </div>
 
-                <div class="col-sm-6 col-xl-3 wow animate__animated animate__slideInUp animate__slower" data-wow-offset="50"
-                    data-wow-delay="0.7s">
+                <div class="col-md-6 col-xl-3 wow animate__animated animate__slideInUp animate__slower"
+                    data-wow-offset="50" data-wow-delay="0.7s">
 
                     <div class="box overflow-hidden" @mouseover="fourthImg = false; fourthTitle = false;"
                         @mouseleave="fourthImg = true; fourthTitle = true;">
@@ -104,7 +104,7 @@
 
             <!-- In Small Screen -->
 
-            <div class="d-block d-sm-none">
+            <div class="d-block d-md-none">
 
                 <div id="carouselValuableSlidesOnly"
                     class="carousel slide wow animate__animated animate__slideInUp animate__slower" data-wow-offset="1"
@@ -113,87 +113,43 @@
                     <div class="carousel-inner">
 
                         <div class="carousel-item active" data-bs-interval="10000">
-
-                            <div class="box overflow-hidden" @mouseover="firstImg = false; firstTitle = false;"
-                                @mouseleave="firstImg = true; firstTitle = true;">
-
-                                <div class="icon">
-                                    <div class="img_wrapper">
-                                        <img :src="firstImg ? firstDefaultImg : firstHoverImg" alt="">
-                                    </div>
-                                    <h3 v-if="firstTitle">الابتكار</h3>
+                            <div class="box">
+                                <div class="img_wrapper mb-3">
+                                    <img src="@/assets/images/about/innovation_small.webp" alt="">
                                 </div>
-
-                                <div class="content">
-                                    <h3>الابتكار</h3>
-                                    <p>ندعم الابتكار في جميع المجالات، ونسعى إلى تطوير حلول جديدة ومبتكرة لتحديات العالم</p>
-                                </div>
-
+                                <h3 class="mb-3">الابتكار</h3>
+                                <p>ندعم الابتكار في جميع المجالات، ونسعى إلى تطوير حلول جديدة ومبتكرة لتحديات العالم</p>
                             </div>
-
                         </div>
 
                         <div class="carousel-item" data-bs-interval="10000">
-
-                            <div class="box overflow-hidden" @mouseover="secondImg = false; secondTitle = false;"
-                                @mouseleave="secondImg = true; secondTitle = true;">
-
-                                <div class="icon">
-                                    <div class="img_wrapper">
-                                        <img :src="secondImg ? secondDefaultImg : secondHoverImg" alt="">
-                                    </div>
-                                    <h3 v-if="secondTitle">المسؤولية</h3>
+                            <div class="box">
+                                <div class="img_wrapper mb-3">
+                                    <img src="@/assets/images/about/responsibility_small.webp" alt="">
                                 </div>
-
-                                <div class="content">
-                                    <h3>المسؤولية</h3>
-                                    <p>نراعي مسؤوليتنا في استثمارات الشركاء من أجل تحقيق أفضل عوائد لهم</p>
-                                </div>
-
+                                <h3 class="mb-3">المسؤولية</h3>
+                                <p>نراعي مسؤوليتنا في استثمارات الشركاء من أجل تحقيق أفضل عوائد لهم</p>
                             </div>
-
                         </div>
 
                         <div class="carousel-item" data-bs-interval="10000">
-
-                            <div class="box overflow-hidden" @mouseover="thirdImg = false; thirdTitle = false;"
-                                @mouseleave="thirdImg = true; thirdTitle = true;">
-
-                                <div class="icon">
-                                    <div class="img_wrapper">
-                                        <img :src="thirdImg ? thirdDefaultImg : thirdHoverImg" alt="">
-                                    </div>
-                                    <h3 v-if="thirdTitle">الشراكة</h3>
+                            <div class="box">
+                                <div class="img_wrapper mb-3">
+                                    <img src="@/assets/images/about/partnership_small.webp" alt="">
                                 </div>
-
-                                <div class="content">
-                                    <h3>الشراكة</h3>
-                                    <p>نؤمن بأهمية الشراكة ، ونسعى إلى تحقيق المنفعة المشتركة</p>
-                                </div>
-
+                                <h3 class="mb-3">الشراكة</h3>
+                                <p>نؤمن بأهمية الشراكة ، ونسعى إلى تحقيق المنفعة المشتركة</p>
                             </div>
-
                         </div>
 
                         <div class="carousel-item" data-bs-interval="10000">
-
-                            <div class="box overflow-hidden" @mouseover="fourthImg = false; fourthTitle = false;"
-                                @mouseleave="fourthImg = true; fourthTitle = true;">
-
-                                <div class="icon">
-                                    <div class="img_wrapper">
-                                        <img :src="fourthImg ? fourthDefaultImg : fourthHoverImg" alt="">
-                                    </div>
-                                    <h3 v-if="fourthTitle">التأثير</h3>
+                            <div class="box">
+                                <div class="img_wrapper mb-3">
+                                    <img src="@/assets/images/about/influence_small.webp" alt="">
                                 </div>
-
-                                <div class="content">
-                                    <h3>التأثير</h3>
-                                    <p>نلتزم بمسؤوليتنا الاجتماعية، ونسعى إلى المساهمة في بناء مجتمع أفضل</p>
-                                </div>
-
+                                <h3 class="mb-3">التأثير</h3>
+                                <p>نلتزم بمسؤوليتنا الاجتماعية، ونسعى إلى المساهمة في بناء مجتمع أفضل</p>
                             </div>
-
                         </div>
 
                     </div>
@@ -277,10 +233,8 @@ export default {
             background-color: transparent;
             box-shadow: 0px 4px 4px 0px $Box-Shadow;
             position: relative;
-
-            @include breakpoints(small) {
-                height: 280px;
-            }
+            // Transition -_-
+            transition: 0.8s ease-in-out;
 
             .icon {
 
@@ -290,7 +244,8 @@ export default {
                 width: 100%;
                 height: 100%;
                 background-color: transparent;
-                transition: 0.5s;
+                // Transition -_-
+                transition: 0.8s ease-in-out;
                 z-index: 1;
 
                 .img_wrapper {
@@ -301,7 +256,8 @@ export default {
                     transform: translate(-50%, -50%);
                     width: 80px;
                     height: 80px;
-                    transition: 0.5s;
+                    // Transition -_-
+                    transition: 0.8s ease-in-out;
 
                     img {
                         width: 100%;
@@ -323,7 +279,8 @@ export default {
                     top: 65%;
                     left: 50%;
                     transform: translate(-50%, 0);
-                    transition: 0.5s;
+                    // Transition -_-
+                    transition: 0.8s ease-in-out;
                 }
 
             }
@@ -334,7 +291,8 @@ export default {
                 height: calc(100% - 100px);
                 text-align: center;
                 padding: 16px;
-                transition: 0.5s;
+                // Transition -_-
+                transition: 0.8s ease-in-out;
                 opacity: 0;
 
                 h3 {
@@ -390,7 +348,51 @@ export default {
 
                         width: 80%;
                         margin: auto;
-                        box-shadow: none;
+
+                        @include breakpoints(x-small) {
+                            height: 290px;
+                        }
+
+                        @include breakpoints(small) {
+                            height: 265px;
+                        }
+
+                        background: $Linear-Blue;
+
+                        padding: 24px 16px;
+
+                        .img_wrapper {
+
+                            width: 80px;
+                            height: 80px;
+
+                            margin: auto;
+
+                            img {
+                                width: 100%;
+                                height: 100%;
+                                object-fit: cover;
+                            }
+                        }
+
+                        h3 {
+                            font-family: 'FFShamelFamily-Bold';
+                            color: $White;
+                            font-size: 20px;
+                            font-style: normal;
+                            font-weight: 400;
+                            line-height: 128%;
+                            text-align: center;
+                        }
+
+                        p {
+                            color: $White;
+                            font-size: 16px;
+                            font-style: normal;
+                            font-weight: 400;
+                            line-height: 180%;
+                            text-align: center;
+                        }
 
                     }
 
@@ -422,4 +424,5 @@ export default {
 
     }
 
-}</style>
+}
+</style>
